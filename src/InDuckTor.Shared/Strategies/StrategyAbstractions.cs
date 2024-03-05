6504c253128971b35881af7dda5c99e0405ca21b
a.a.ebel <a.a.ebel@tinkoff.ru>
2024-03-05 23:55:04 +0300

    test

--- src/InDuckTor.Shared/Strategies/StrategyAbstractions.cs
+++ src/InDuckTor.Shared/Strategies/StrategyAbstractions.cs
@@ -12,9 +12,6 @@ public interface ICommand<in TInput, TOutput> : IStrategy<TInput, TOutput>;
 public interface IQuery<in TInput, TOutput> : IStrategy<TInput, TOutput>;
 
 // todo : check how contravariance TInput of IStrategy<in TInput, TOutput> behave in interceptors
-/// <summary>
-/// 
-/// </summary>
 /// <remarks><typeparamref name="TInput"/> инвариантен в отличии от аналогичного параметра в <see cref="IStrategy{TInput,TOutput}"/></remarks>
 public interface IStrategyInterceptor<TInput, TOutput>
 {
